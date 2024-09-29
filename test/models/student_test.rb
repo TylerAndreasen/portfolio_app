5ee1dac092c4ddb84137f2c835632ca833a312ef
TylerAndreasen <45052003+TylerAndreasen@users.noreply.github.com>
2024-09-28 19:14:53 -0600

    Completed a total of 7 tests
    Test index 6 does not work as it assigns a duplicate email to a student, though I am unsure if this is the fault of the controller, a limit of the testing framework, or something else.

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -8,6 +8,7 @@ class StudentsTest < ActiveSupport::TestCase
     assert Student.create!(first_name: "Tyler", last_name: "Andreasen", school_email: "tand@msudenver.edu", major: "Debugging", graduation_date: "2024/09/23")
   end
 
+=begin
   ##
   # I would like to implement tests that verify that a stink is made by rails
   # if for example the first_name field is blank, but I cannot for the
@@ -18,6 +19,7 @@ class StudentsTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+=end
 
   # Test 1: No Data Student
   test "student_has_no_data" do
@@ -31,30 +33,29 @@ class StudentsTest < ActiveSupport::TestCase
     assert_not student0.save
   end
 
-  # Test 3: Clean Read of Student
-  test "read_valid_student" do
-    assert(:one, "Indoril exists!")
+  # Test 3: Ensure Spadetail has expected email.
+  test "spadetails_email_matches_literal" do
+    assert_equal "cspadeta@msudenver.edu", students(:spadetail).school_email
   end
 
+
+  # Test 4: Find by Name that doesn't exist
+  test "student_record_of_name_doesn't_exist" do
+    assert_raise(StandardError) { students(:sotha) }
+  end
+
+  # Test 5: Successfully Update Student Name
+  test "assign_student_new_name" do
+    students(:spadetail).first_name = "Champion"
+    assert_equal "Champion", students(:spadetail).first_name
+  end
+
+  # Test 6: Attempt to Assign a duplicate email.
+  # This currently does not function as intended, as the duplicate email still assigns.
+  test "school_email_updated_to_existing_value" do
+    assert students(:spadetail).school_email = "inerevar@msudenver.edu"
+  end
 =begin
-  # Test 4: No First Name Student
-  test "student_has_no_name" do
-    student0 = Student.new(first_name: "", last_name: "Andreasen", school_email: "tand@msudenver.edu", major: "Debugging", graduation_date: "2024/09/23")
-    assert_not student0.save
-  end
-
-  # Test 5: No First Name Student
-  test "student_has_no_name" do
-    student0 = Student.new(first_name: "", last_name: "Andreasen", school_email: "tand@msudenver.edu", major: "Debugging", graduation_date: "2024/09/23")
-    assert_not student0.save
-  end
-
-  # Test 6: No First Name Student
-  test "student_has_no_name" do
-    student0 = Student.new(first_name: "", last_name: "Andreasen", school_email: "tand@msudenver.edu", major: "Debugging", graduation_date: "2024/09/23")
-    assert_not student0.save
-  end
-  
   # Test 7: No First Name Student
   test "student_has_no_name" do
     student0 = Student.new(first_name: "", last_name: "Andreasen", school_email: "tand@msudenver.edu", major: "Debugging", graduation_date: "2024/09/23")
